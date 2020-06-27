3cf6e9a73f832a4e80159fc01291bf9274d0dad0
Abhishek Aggarwal <abhishek.aggarwal02.93@gmail.com>
2020-06-27 18:30:03 +0530

    Added ServletContext

--- src/org/abhishek/servlet/SimpleServlet.java
+++ src/org/abhishek/servlet/SimpleServlet.java
@@ -3,6 +3,7 @@ package org.abhishek.servlet;
 import java.io.IOException;
 import java.io.PrintWriter;
 
+import javax.servlet.ServletContext;
 import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
@@ -23,16 +24,23 @@ public class SimpleServlet extends HttpServlet {
 	 */
 	protected void doGet(HttpServletRequest request, HttpServletResponse response)
 			throws ServletException, IOException {
-		// TODO Auto-generated method stub
+		
+		//For maintaining a session(Per user/browser)
 		HttpSession session = request.getSession();
+		
+		//At servlet level(for all browsers)
+		ServletContext context = request.getServletContext();
+		
 		PrintWriter out = response.getWriter();
 		String username = request.getParameter("name");
 		if (username != null && !username.isEmpty()) {
 			session.setAttribute("loggedInUser", username);
+			context.setAttribute("loggedInUser", username);
 		}
 		// response.getWriter().append("Served at: ").append(request.getContextPath());
-		out.println("Hello, " + username);
-		out.println("Session user is: " + (String) session.getAttribute("loggedInUser"));
+		out.println("Request parameter has Username as: " + username);
+		out.println("Session parameter has Username as: " + (String) session.getAttribute("loggedInUser"));
+		out.println("Context parameter has Username as: " + (String) context.getAttribute("loggedInUser"));
 		System.out.println("Hello Get Method!!");
 	}
 
